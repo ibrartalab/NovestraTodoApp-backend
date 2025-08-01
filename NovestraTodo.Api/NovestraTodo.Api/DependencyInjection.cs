cb301f23b085da4b6863fb0e7b45981e69955108
Ibrar talab <ibrartalabkhan@gmail.com>
2025-08-01 15:43:55 +0500

    create ASP.NET Web API app using clean architecture and implement dependencyInjection

---
+++ NovestraTodo.Api/NovestraTodo.Api/DependencyInjection.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using NovestraTodo.Application;
+using NovestraTodo.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NovestraTodo.Api
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddAppDI(this IServiceCollection services)
+        {
+            // Register infrastructure services here
+            services.AddApplicationDI()
+                .AddInfrastructureDI();
+            return services;
+        }
+    }
+}
